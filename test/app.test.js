34a5be67aa65500b2cde5b4d8a78d10d94d2d7ff
cruzayye <johnnyc010392@gmail.com>
2019-01-22 12:40:30 -0800

    changed url includes to url pathname includes

--- test/app.test.js
+++ test/app.test.js
@@ -4,13 +4,14 @@ const app = require('../lib/app');
 describe('Pirates API', () => {
     it('checks /api/pinguins request', () => {
         return request(app)
-            .get('./api/pinguins')
+            .get('/api/pinguins')
             .send([
                 'bernice', 
                 'bernard'
             ])
             .then(res => {
-                expect(res.body).toContain('bernice');
+              console.log('this this', res.body);
+                expect(res.header).toContain('bernice');
             });
     });
 });
